468ba2be404063416318c4545a77b29ae4b7027f
Billy McCarthy <billy@reshippie.net>
2026-05-23 19:32:07 -0700

    Improved documentation and logging

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # crilyrion-collection
-Dump the list of albums from Lyrion and upload to a file in an S3 bucket
+It's my Criterion Collection.  Actually, it's my whole collection.
+Dump the list of albums from Lyrion and upload to a markdown file in an B2/S3 bucket.
+In my case, the bucket contains my Obsidian vault.  Now I can have a list of all CDs I own
+synced to my phone.
+
+# Example .crilerion file
+```
+[default]
+keyid = a0a0a0a0a0a0a0a0a0a0a0a0a
+applicationkey = Keys2daVault
+bucket_endpoint = https://s3.us-east-0005.backblazeb2.com
+bucket_name = ObsidianVault
+db_path = /path/to/your/lyrion/cache/library.db
+md_file_path = /home/username/collection.md
+remote_path = cd-bucket
+# cleanup = yes # uncomment to remove file
+```
--- collection.py
+++ collection.py
@@ -1,14 +1,31 @@
 #! /usr/bin/env python
 
+import argparse
 import boto3
 import configparser
 import json
+import logging
 import os
 import sqlite3
 
+
+def create_parser():
+    '''
+        Define the command line arguments.
+    '''
+    parser = argparse.ArgumentParser()
+    default_config = f"{os.environ['HOME']}/.crilyrion"
+    parser.add_argument('-c', '--config', default=default_config, help='Where to find your config file.')
+    parser.add_argument('-v', '--verbose', action='store_true', help="Do you want to know what's going on?")
+    return parser.parse_args()
+
 def write_markdown(config):
+    '''
+        Dump the data from SQLite into a nice markdown file.
+    '''
+    logging.info(f"Reading data from {config['db_path']}.")
     cur = sqlite3.connect(config['db_path']).cursor()
-    data = cur.execute("select contributors.name, albums.title from albums inner join  contributors on albums.contributor=contributors.id").fetchall()
+    data = cur.execute('select contributors.name, albums.title from albums inner join  contributors on albums.contributor=contributors.id').fetchall()
     collection = {}
     for artist, album in data:
         if artist in collection:
@@ -16,6 +33,7 @@ def write_markdown(config):
         else:
             collection[artist] = [album]
 
+    logging.info(f"Writing data to {config['md_file_path']}.")
     with open(config['md_file_path'], 'w') as collectfile:
         json.dump(collection, collectfile, ensure_ascii=False, indent=2)
 
@@ -34,12 +52,24 @@ def upload_file(config, b2):
     res = b2.Bucket(bucket_name).upload_file(file_path, remote_path)
 
 def main():
+    args = create_parser()
+    if args.verbose:
+        logging.basicConfig(level='INFO')
     configs = configparser.ConfigParser()
-    configs.read('/home/billy/.obs-albums')
+    configs.read(args.config)
     config = configs['default']
     write_markdown(config)
     b2 = get_b2_connection(config)
+    logging.info(f"Uploading file to {config['bucket_endpoint']}/{config['remote_path']}.")
     upload_file(config, b2)
+    try:
+        if config['cleanup'].lower() == 'yes':
+            logging.info(f"Deleting markdown file {config['md_file_path']}.")
+            os.unlink(config['md_file_path'])
+        else:
+            logging.info(f"Leaving markdown file {config['md_file_path']} alone.")
+    except KeyError:
+        logging.info(f"Leaving markdown file {config['md_file_path']} alone.")
     
 if __name__ == '__main__':
     main()
